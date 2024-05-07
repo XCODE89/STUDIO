6cab52483b92997f2a6ebb85ff8e427ec177c28a
XCODE89 <maph0989@gmail.com>
2024-05-06 20:16:39 -0400

    Home complete

--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -1,14 +1,23 @@
-import NavBar from '../../components/navigation/NavBar'
+
 import Layout from '../../hocs/layouts/Layout'
 import React from 'react'
 import Footer from '../../components/navigation/Footer'
+import Header from '../../components/home/Header'
+import Incentives from '../../components/home/Incentives'
+import UseCases from '../../components/home/UseCases'
+import Features from '../../components/home/Features'
+import CTA from '../../components/home/CTA'
+import Clients from '../../components/home/Clients'
 
 const Home = () => {
   return (
     <Layout>
-      <NavBar/>
-      <div>Home</div>
-      <Footer/>
+      <Header/>
+      <Incentives/>
+      <UseCases/>
+      <Features/>
+      <CTA/>
+      <Clients/>
     </Layout>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,14 @@ export default {
     extend: {
       colors: {
         "orange-button": "#ff561e"
-      }
+      },
     },
+    boxShadow : {
+      navbar : "0 10px 8px 0 rgba(3, 3, 4, 0.03), 0 1px 2px -1px rgba(3, 3, 4, 0.03)",
+    }
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
 
